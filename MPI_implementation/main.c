23b4f6a69dbc35c44ef831e804ed8267fefd82f7
Vangelis <vtsiat1@gmail.com>
2017-07-10 15:48:24 +0300

    Hello!

---
+++ MPI_implementation/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 10, 2017
+ *      Author: vangelis
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv){
+	printf("hello!\n");
+	return 0;
+}
