b95a6c7ea48d5498fb0e32dd37a3cd68e991238e
Vangelis <vtsiat1@gmail.com>
2017-10-03 22:38:29 +0300

    scalabilty scripts changes
    proste8hkan perissoteres eketeleseis me diaforetika flags

--- scripts/benchmarks/mpi_openmp_bench.sh
+++ scripts/benchmarks/mpi_openmp_bench.sh
@@ -2,43 +2,50 @@
 
 clear 
 echo "Execution 1"
-echo "Processors: 4, Threads: 2, Dimension: 100x100, Loops: 100"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 100 -l 100;
+echo "Processors: 4, Dimension: 30x30, Loops: 100"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
 echo
 echo "Execution 2"
-echo "Processors: 4, Threads: 2, Dimension: 100x100, Loops: 1000"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 100 -l 1000;
+echo "Processors: 4, Dimension: 30x30, Loops: 1000"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
 echo
 echo "Execution 3"
-echo "Processors: 4, Threads: 4, Dimension: 100x100, Loops: 100"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 100 -l 100;
+echo "Processors: 4, Dimension: 300x300, Loops: 100"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
 echo
 echo "Execution 4"
-echo "Processors: 4, Threads: 4, Dimension: 100x100, Loops: 1000"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 100 -l 1000;
+echo "Processors: 4, Dimension: 300x300, Loops: 1000"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
 echo
 echo "Execution 5"
-echo "Processors: 4, Threads: 2, Dimension: 1000x1000, Loops: 100"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 1000 -l 100;
+echo "Processors: 4, Dimension: 3000x3000, Loops: 100"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
 echo
 echo "Execution 6"
-echo "Processors: 4, Threads: 2, Dimension: 1000x1000, Loops: 1000"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 1000 -l 1000;
-echo
-echo "Execution 7"
-echo "Processors: 4, Threads: 4, Dimension: 1000x1000, Loops: 100"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 1000 -l 100;
+echo "Processors: 4, Dimension: 3000x3000, Loops: 1000"
+mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
 echo
 echo "Execution 8"
-echo "Processors: 4, Threads: 4, Dimension: 1000x1000, Loops: 1000"
-mpiexec -n 4 -f machines -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 1000 -l 1000;
-
-
-reduction
-1.096354
-0.858884
-0.900564
-0.987604
-
-
-no reduction
+echo "Processors: 4, Dimension: 30x30, Loops: 100"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
+echo
+echo "Execution 9"
+echo "Processors: 4, Dimension: 30x30, Loops: 1000"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
+echo
+echo "Execution 10"
+echo "Processors: 4, Dimension: 300x300, Loops: 100"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
+echo
+echo "Execution 11"
+echo "Processors: 4, Dimension: 300x300, Loops: 1000"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
+echo
+echo "Execution 12"
+echo "Processors: 4, Dimension: 3000x3000, Loops: 100"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
+echo
+echo "Execution 13"
+echo "Processors: 4, Dimension: 3000x3000, Loops: 1000"
+mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
+echo
