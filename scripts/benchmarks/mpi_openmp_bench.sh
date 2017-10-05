5b8be1dc041d6b5dd6999dc88fa54bad50403a00
Evangelos Tsiatouras <sdi1200185@linux29.di.uoa.gr>
2017-10-05 21:21:16 +0300

    allages sto script openmp /allax8hke to machines sto openmp

--- scripts/benchmarks/mpi_openmp_bench.sh
+++ scripts/benchmarks/mpi_openmp_bench.sh
@@ -3,49 +3,49 @@
 clear 
 echo "Execution 1"
 echo "Processors: 4, Dimension: 30x30, Loops: 100"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
 echo
 echo "Execution 2"
 echo "Processors: 4, Dimension: 30x30, Loops: 1000"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
 echo
 echo "Execution 3"
 echo "Processors: 4, Dimension: 300x300, Loops: 100"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
 echo
 echo "Execution 4"
 echo "Processors: 4, Dimension: 300x300, Loops: 1000"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
 echo
 echo "Execution 5"
 echo "Processors: 4, Dimension: 3000x3000, Loops: 100"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
 echo
 echo "Execution 6"
 echo "Processors: 4, Dimension: 3000x3000, Loops: 1000"
-mpiexec -n 4 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
 echo
 echo "Execution 8"
-echo "Processors: 4, Dimension: 30x30, Loops: 100"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
+echo "Processors: 9, Dimension: 30x30, Loops: 100"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
 echo
 echo "Execution 9"
-echo "Processors: 4, Dimension: 30x30, Loops: 1000"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
+echo "Processors: 9, Dimension: 30x30, Loops: 1000"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
 echo
 echo "Execution 10"
-echo "Processors: 4, Dimension: 300x300, Loops: 100"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
+echo "Processors: 9, Dimension: 300x300, Loops: 100"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
 echo
 echo "Execution 11"
-echo "Processors: 4, Dimension: 300x300, Loops: 1000"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
+echo "Processors: 9, Dimension: 300x300, Loops: 1000"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
 echo
 echo "Execution 12"
-echo "Processors: 4, Dimension: 3000x3000, Loops: 100"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
+echo "Processors: 9, Dimension: 3000x3000, Loops: 100"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
 echo
 echo "Execution 13"
-echo "Processors: 4, Dimension: 3000x3000, Loops: 1000"
-mpiexec -n 9 -bind-to socket ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
+echo "Processors: 9, Dimension: 3000x3000, Loops: 1000"
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
 echo
--- scripts/paraver/omptrace.sh
+++ scripts/paraver/omptrace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export EXTRAE_HOME=/usr/local/extrae
-export EXTRAE_CONFIG_FILE=../extrae.xml
+export EXTRAE_CONFIG_FILE=extrae.xml
 export LD_PRELOAD=$EXTRAE_HOME/lib/libomptrace.so
 export EXE=$2 # $2: second argument given by terminal
 export TRACENAME=${EXE}.prv
