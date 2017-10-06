a030f3e0a6c7902c877ba013d7684c894c69c1fe
Vangelis <vtsiat1@gmail.com>
2017-10-06 17:01:09 +0300

    use 4 threads

--- scripts/benchmarks/mpi_openmp_bench.sh
+++ scripts/benchmarks/mpi_openmp_bench.sh
@@ -27,25 +27,25 @@ mpiexec -f ../../MPI_OpenMP_implementation/machines -n 4 -bind-to core ../../MPI
 echo
 echo "Execution 7"
 echo "Processors: 9, Dimension: 30x30, Loops: 100"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 30 -l 100;
 echo
 echo "Execution 8"
 echo "Processors: 9, Dimension: 30x30, Loops: 1000"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 30 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 30 -l 1000;
 echo
 echo "Execution 9"
 echo "Processors: 9, Dimension: 300x300, Loops: 100"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 300 -l 100;
 echo
 echo "Execution 10"
 echo "Processors: 9, Dimension: 300x300, Loops: 1000"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 300 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 300 -l 1000;
 echo
 echo "Execution 11"
 echo "Processors: 9, Dimension: 3000x3000, Loops: 100"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 100;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 3000 -l 100;
 echo
 echo "Execution 12"
 echo "Processors: 9, Dimension: 3000x3000, Loops: 1000"
-mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 2 -d 3000 -l 1000;
+mpiexec -f ../../MPI_OpenMP_implementation/machines -n 9 -bind-to core ../../MPI_OpenMP_implementation/game_of_life -t 4 -d 3000 -l 1000;
 echo
